7bb914003957602d93a419dd497395d005e004cd
EC2 Default User <ec2-user@ip-172-31-91-148.ec2.internal>
2025-03-01 07:52:35 +0000

    homework-11

---
+++ Homework-11/introduce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name="Gulzina"
+age=35
+favorite_language="Python"
+hobby="hiking"
+dream="explore the world"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby. As a $age-year-old, I believe it's never too late to follow your dreams.
+My favorite programming  language is $favorite_language. I code in $favorite_language almost every day! 
+One day, I hope my love for $favorite_language will help  me achive my dream of $dream. When I'm not busy working toward $dream, you'll find my $hobby to relax.
+That's a little about me, $name a $age-year old who loves $favorite_language, enjoys $hobby, and dreams od $dream!"
+
+
